b1699c7e82d302cc731b8a69a900181f9a3f60b5
Brayden Moon <crazywolf132@gmail.com>
2023-12-12 13:35:03 +1100

    chore: removed old test code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
-import Lexer from './lexer'
-import Parser from './parser'
-import Node from './parser/node'
-export type * from './types'
+import Lexer from './lexer';
+import Parser from './parser';
+import Node from './parser/node';
+export type * from './types';
 
-import fs from 'node:fs';
-import { inspect } from 'util'
 
 export default (css: string): Node => {
    const lexer = new Lexer(css)
@@ -14,36 +12,5 @@ export default (css: string): Node => {
 }
 
 export {
-   Lexer,
-   Parser,
-   Node
-}
-
-(() => {
-
-   const css = `{
-      background-color: red;
-      color: blue;
-
-      @variants {
-         Secondary {
-            background-color: black;
-            color: white;
-
-            Text {
-               color: white;
-               font-size: 1rem;
-               font-weight: bold;
-            }
-         }
-      }
-   }`
-
-   const lexer = new Lexer(css)
-   lexer.tokenize()
-
-   const res = new Parser(lexer).parse();
-   console.log(inspect(res, false, null, true));
-   // fs.writeFileSync('./result.json', JSON.stringify(res, null, 2));
-
-})();
+   Lexer, Node, Parser
+};
